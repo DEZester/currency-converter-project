9d28b10a513975a5811c7dffcd8c831aa16fdb35
DEZester <aromudez@gmail.com>
2023-08-02 20:57:29 +0300

    Finish app

--- src/CurrencyInput.jsx
+++ src/CurrencyInput.jsx
@@ -1,14 +1,16 @@
 import './currencyInput.css';
 
 const CurrencyInput = (props) =>{
-	return <div className="group">
-		<input type="text" value={props.amount}  />
-		<select value={props.currency} >
-			{props.currencies.map((currency => (
-				<option value={currency}>{currency}</option>
-			)))}
-		</select>
-	</div>
+	return (
+		<div className="group">
+			<input type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
+			<select value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
+				{props.currencies.map((currency => (
+					<option value={currency}>{currency}</option>
+				)))}
+			</select>
+		</div>
+	);
 }
 
 export default CurrencyInput
